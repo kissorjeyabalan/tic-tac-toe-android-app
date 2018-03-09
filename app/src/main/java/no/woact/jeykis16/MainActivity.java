3a3ca2478871a19da8f79cb3e92ffaca03d28e91
Kissor Jeyabalan <git@kij.no>
2018-03-09 12:08:11 +0100

    added butterknife

--- app/src/main/java/no/woact/jeykis16/MainActivity.java
+++ app/src/main/java/no/woact/jeykis16/MainActivity.java
@@ -7,11 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
